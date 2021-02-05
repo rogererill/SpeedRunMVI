782be407ef8f3bf76123e8441583ac3b0280217a
Roger Erill <roger.erill@gmail.com>
2021-02-05 19:43:44 +0100

    Add rendering state for GameRunActivity

--- feature/gamedetail/src/main/java/com/erill/roger/feature/gamedetail/presentation/GameRunActivity.kt
+++ feature/gamedetail/src/main/java/com/erill/roger/feature/gamedetail/presentation/GameRunActivity.kt
@@ -2,15 +2,22 @@ package com.erill.roger.feature.gamedetail.presentation
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.erill.roger.commons.entities.Game
 import com.erill.roger.feature.gamedetail.databinding.ActivityGameDetailBinding
 import com.erill.roger.feature.gamedetail.di.GameRunComponentProvider
+import dagger.BindsInstance
 import dagger.Subcomponent
+import io.reactivex.Observable
+import javax.inject.Inject
+import javax.inject.Named
 
-class GameRunActivity : AppCompatActivity() {
+class GameRunActivity : AppCompatActivity(), GameRunView {
 
     companion object {
         private const val EXTRA_GAME = "EXTRA_GAME"
@@ -24,6 +31,9 @@ class GameRunActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameDetailBinding
 
+    @Inject
+    lateinit var presenter: GameRunPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameDetailBinding.inflate(layoutInflater)
@@ -34,6 +44,8 @@ class GameRunActivity : AppCompatActivity() {
 
         (applicationContext as GameRunComponentProvider).gameRunComponent
             .gameRunActivityBuilder()
+            .withView(this)
+            .withGameId(game.id)
             .build()
             .inject(this)
 
@@ -43,12 +55,49 @@ class GameRunActivity : AppCompatActivity() {
             .centerCrop()
             .placeholder(android.R.color.darker_gray)
             .into(binding.ivGame)
+
+        lifecycle.addObserver(presenter)
+    }
+
+    override val userIntents: Observable<GameRunViewIntent>
+        get() = Observable.empty() // TODO: Implement refresh
+
+    override fun render(state: GameRunState) {
+        when (state) {
+            GameRunState.Loading -> {
+                binding.progress.visibility = View.VISIBLE
+                binding.buttonSeeVideo.isEnabled = false
+            }
+            GameRunState.Error -> {
+                binding.progress.visibility = View.GONE
+                binding.buttonSeeVideo.isEnabled = false
+                binding.tvPlayerName.text = "Unknown"
+                binding.tvTime.text = "Unknown"
+            }
+            is GameRunState.Data -> {
+                binding.progress.visibility = View.GONE
+                binding.tvPlayerName.text = state.gameRun.playerName ?: ""
+                binding.tvTime.text = DateUtils.formatElapsedTime(state.gameRun.timeInSeconds.toLong())
+                binding.buttonSeeVideo.isEnabled = true
+                binding.buttonSeeVideo.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse(state.gameRun.videoUrl)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
     @Subcomponent
     interface Component {
         @Subcomponent.Builder
         interface Builder {
+
+            @BindsInstance
+            fun withView(view: GameRunView): Builder
+
+            @BindsInstance
+            fun withGameId(@Named("gameId") gameId: String): Builder
 
             fun build(): Component
         }
